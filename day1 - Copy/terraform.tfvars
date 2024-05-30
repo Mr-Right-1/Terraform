981350da77b942f6110aa4347a4ecdf7f20fb206
Mr-Righy-1 <sardargurvinder2113@gmail.com>
2024-05-30 15:12:50 +0530

    mdmdmdm

---
+++ day1 - Copy/terraform.tfvars
@@ -0,0 +1,3 @@
+ami_id = "ami-0e4fd655fb4e26c30"
+instance_type = "t2.micro"
+key_name = "mumbai"
---
+++ day1 - Copy/variable.tf
@@ -0,0 +1,18 @@
+variable "ami_id" {
+    description = "thsi my ami"
+    type = string
+    default = ""
+  
+}
+variable "instance_type" {
+    description = "vcbszunxo"
+    type = string
+    default = ""
+
+}
+variable "key_name" {
+    description = "vfmdfoefn"
+    type = string
+    default = ""
+  
+}
